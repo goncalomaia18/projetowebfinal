b940cb959c8c143dbef354843f506e010f81e7d4
hf <hf@isep>
2023-05-24 15:33:33 +0100

    Correções

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -5,22 +5,24 @@ const passwInput = document.querySelector("#pass")
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    //util vazio
+    //utilizador e password vazio
+    if(utiliInput.value === "" && passwInput.value==="") {
+        alert("Por favor preencha as suas credenciais.");
+        return;
+    }
+    
+    //utilizador vazio
     if(utiliInput.value === "") {
-        alert("Por favor preencha o seu utilizador");
+        alert("Por favor preencha o seu nome de utilizador.");
         return;
     }
 
+    //password vazio
+    if(passwInput.value === "") {
+        alert("Por favor preencha a sua palavra-passe.");
+        return;
+    }
 
     //se os campos estiverem preenchidos
     form.submit();
-
-    //verifica se o email esta preecnhido e válido
-    if(passwInput.value === "") {
-        alert("Por favor preencha a sua Palavra-Passe");
-        return;
-    }
 });
-
-
-
